e54cdc30af13dcb1b1605ac920d9175823a1f829
Blair Davidson <blair.davidson@icetana.com.au>
2018-11-12 14:36:16 +0800

    check gcc 6

--- include/pulley/algorithms/for_each.hpp
+++ include/pulley/algorithms/for_each.hpp
@@ -3,20 +3,39 @@
 
 #include <tuple>
 #include <utility>
+#include <pulley/algorithms/tail.hpp>
 
 namespace pulley {
     namespace algorithms {
         namespace detail {
+
+#if defined(__GNUC__) && (__GNUC___ >= 7) 
             template<typename... Ts, size_t... Is, typename F>
             constexpr void for_each(std::tuple<Ts...> const& t, std::index_sequence<Is...> is, F&& f) {
                 std::initializer_list<int> i{(f(std::get<Is>(t)), 0)... };
             }
+#else
+	    template<typename T, typename... Ts, typename F>
+	    struct for_each<std::tuple<T, Ts...>, F>
+            {
+		 static constexpr void apply(std::tuple<T, Ts...>& t, F f) noexcept(noexcept(f(std::get<0>(t))) && std::is_nothrow_copy_assignable<F>::value){
+		     f(std::get<0>(t));
+		     auto tt = tail(t);
+		     detail::for_each<std::remove_reference_t<decltype(tt)>, F>::apply(tt, f);
+	         }
+	    };
+#endif
+
         
         }
         
         template<typename... Ts, typename F>
         constexpr void for_each(std::tuple<Ts...> const& t, F&& f) {
+#if defined(__GNUC__) && (__GNUC___ >= 7) 
             detail::for_each(t, std::make_index_sequence<std::tuple_size<std::tuple<Ts...>>::value>{}, std::forward<F>(f));
+#else
+            detail::for_each<std::remove_reference_t<decltype(t)>, F>::apply(t, f);
+#endif
         }
     }
 }
